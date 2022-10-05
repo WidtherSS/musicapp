d609ef6304879109955338f777a47cb089042f97
Adam <adamchatila2020@gmail.com>
2022-10-06 01:30:24 +0200

    adding song cards

--- src/pages/Discover.jsx
+++ src/pages/Discover.jsx
@@ -14,7 +14,9 @@ const Discover = () => {
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center 
             sm:flex-row flex-col mt-4 mb-10">
-                <h2 className="font-bold text-3xl text-white texy-left">Discover {genreTitle}</h2>
+                <h2 className="font-bold text-3xl animate-slideup text-[#ee0979]   texy-left">
+                    Discover {genreTitle}
+                </h2>
                 <select
                 onChange={() =>{}}
                 value=""
