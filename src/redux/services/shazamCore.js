e9c9223d91be0d9a8f1b5640742b4a18ec911e3a
Adam <adamchatila2020@gmail.com>
2022-10-10 15:51:45 +0200

    ADD SONG DETAILS with shazamCore

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -12,10 +12,13 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
             }
         }),
         endpoints:(builder) => ({
-             getTopCharts:(builder.query({query:() => '/charts/world'}))
-        })
+            getTopCharts:(builder.query({query:() => '/charts/world'})),
+            getSongDetails:(builder.query({query:(songid)=> `/tracks/details?track_id=${songid}`})),
+
+        }),
     });
 
     export const {
         useGetTopChartsQuery,
+        useGetSongDetailsQuery
     } = shazamCoreApi
